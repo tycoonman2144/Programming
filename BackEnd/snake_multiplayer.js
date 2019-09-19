4626a9fcb49e70a1f995d005ed1480955ce10f9e
tycoonman2144 <43999884+tycoonman2144@users.noreply.github.com>
2019-09-19 10:44:31 -0600

    Update snake_multiplayer.js

--- BackEnd/snake_multiplayer.js
+++ BackEnd/snake_multiplayer.js
@@ -140,9 +140,11 @@ app.get('/startMultiPlayerGame/:RoomID', function (req, res) {
 });
 
 function CheckIfExited() {
-	console.log("got in check function");
 	for(var i = 0; i < rooms.length; i++){
 		for(var j = 0; j < rooms[i].snakes.length; j++) {
+			console.log("TD: " + rooms[i].snakes[j].timeDiffrence);
+			console.log("Time Stamp: " + rooms[i].snakes[j].timeStamp);
+			console.log("DateNow: " + Date.now());
 			console.log("Time Diffrence: " + (Date.now() - rooms[i].snakes[j].timeDiffrence) - rooms[i].snakes[j].timeStamp);
 			if((Date.now() - rooms[i].snakes[j].timeDiffrence) - rooms[i].snakes[j].timeStamp >= 4000){ //them most likley exited
 				rooms[i].snakes.splice(j, 1);
